7ef0a6308fff76c377502c8d79384806f7492315
Camilo Cardenas <j.cardenasc@uniandes.edu.co>
2020-10-08 21:46:27 -0500

    Fix minor bug

--- cypress/captura.js
+++
@@ -1,7 +0,0 @@
-describe('Taller 6 - Captura de screenshots', function() {
-    it('Generando paleta de colores', function() {
-      cy.visit('https://cmartinezbjmu.github.io/miso-4208-vrt/palette.html')
-      cy.contains('Generar nueva paleta').click()
-      cy.screenshot();		  
-    })
-})
