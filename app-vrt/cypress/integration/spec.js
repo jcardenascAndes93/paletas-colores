4fc224ed61c27812627696067094e1e8a4438b07
jcardenascAndes93 <54183886+jcardenascAndes93@users.noreply.github.com>
2020-10-11 14:15:15 -0500

    Updates color palette app url

--- app-vrt/cypress/integration/spec.js
+++ app-vrt/cypress/integration/spec.js
@@ -1,6 +1,6 @@
 describe('Taller 7 - Captura de screenshots', function() {
     it('Generando paleta de colores', function() {
-      cy.visit('https://cmartinezbjmu.github.io/miso-4208-vrt/palette.html')
+      cy.visit('https://jcardenascandes93.github.io/colors-palette/')
       cy.wait(1000)
       cy.screenshot('screen-1', {timeout: 50000})
       cy.contains('Generar nueva paleta').click()
